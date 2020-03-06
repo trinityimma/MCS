b8d470da1cfb6865823327f2249d024dfc687382
John Ansa <johnansa1@yahoo.com>
2020-03-06 18:15:46 +0100

    Add MCSLib project for MCS implementation

---
+++ MCSLib/IPDF.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MCSLib
+{
+    public interface IPDF
+    {
+        IList<double> SingleDistribution(int interval, int iteration, params double[] args);
+        IList<double> UniformDistribution(int interval, int iteration, params double[] args);
+        IList<double> TriangularDistribution(int interval, int iteration, params double[] args);
+    }
+}
