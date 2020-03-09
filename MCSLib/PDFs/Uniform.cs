7b5025ec6064473d197e66c987b0024389cd6fc9
John Ansa <johnansa1@yahoo.com>
2020-03-09 12:35:23 +0100

    implemented uniform and triangular distribution

--- MCSLib/PDFs/Uniform.cs
+++ MCSLib/PDFs/Uniform.cs
@@ -7,32 +7,68 @@ namespace MCSLib.PDFs
     {
         public override IEnumerable<double> GetDistribution(int iteration, params double[] uncertainties)
         {
-            throw new NotImplementedException();
+            return ValidateInput(iteration, uncertainties);
         }
 
-        public override IEnumerable<double> GetDistribution(int iteration, IEnumerable<double> uncertainties)
+        public override IEnumerable<double> GetDistribution(int iteration, List<double> uncertainties)
         {
-            throw new NotImplementedException();
+            return ValidateInput(iteration, uncertainties.ToArray());
         }
 
         public override IEnumerable<double> GetDistribution(Params args)
         {
-            throw new NotImplementedException();
+            return ValidateInput(args.Iteration, args.Uncertainties);
         }
 
         public override IEnumerable<double> GetDistribution(Func<double, double> action, int iteration, params double[] args)
         {
-            throw new NotImplementedException();
+            return ValidateInputWithDelegate(action, iteration, args);
         }
 
-        public override IEnumerable<double> GetDistribution(Func<double, double> action, int iteration, IEnumerable<double> args)
+        public override IEnumerable<double> GetDistribution(Func<double, double> action, int iteration, List<double> args)
         {
-            throw new NotImplementedException();
+            return ValidateInputWithDelegate(action, iteration, args.ToArray());
         }
 
         public override IEnumerable<double> GetDistribution(Func<double, double> action, Params args)
         {
-            throw new NotImplementedException();
+            return ValidateInputWithDelegate(action, args.Iteration, args.Uncertainties);
+        }
+
+        private IEnumerable<double> ValidateInput(int iteration, double[] uncertainties)
+        {
+            if (uncertainties.Length != 2)
+                throw new ArgumentOutOfRangeException("uncertainties", "uncertainties for uniform distribution must have two values, minValue and maxValue");
+            return GetUniformDistribution(iteration, uncertainties[0], uncertainties[1]);
+        }
+        private IEnumerable<double> ValidateInputWithDelegate(Func<double, double> action,int iteration, double[] uncertainties)
+        {
+            if (uncertainties.Length != 2)
+                throw new ArgumentOutOfRangeException("uncertainties", "uncertainties for uniform distribution must have two values, minValue and maxValue");
+            return GetUniformDistribution(action, iteration, uncertainties[0], uncertainties[1]);
+        }
+        private IEnumerable<double> GetUniformDistribution(int iteration, double minValue, double maxValue)
+        {
+            Random rand = new Random();
+            var results = new List<double>();
+            for (int i = 0; i < iteration; i++)
+            {
+                double ans = minValue + (maxValue - minValue) * rand.NextDouble();
+                results.Add(ans);
+            }
+            return results;
+        }
+
+        private IEnumerable<double> GetUniformDistribution(Func<double, double> action, int iteration, double minValue, double maxValue)
+        {
+            Random rand = new Random();
+            var results = new List<double>();
+            for (int i = 0; i < iteration; i++)
+            {
+                double ans = minValue + (maxValue - minValue) * rand.NextDouble();
+                results.Add(action(ans));
+            }
+            return results;
         }
     }
 }
